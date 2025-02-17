cf7e6bef8a17e4f14dc9d3de487116524d691f85
Yaroslav <elektroyar@yandex.ru>
2025-02-17 06:33:17 +0300

    feat(Utils): add aggregated utilities header with documentation

--- include/log-it/parts/Utils.hpp
+++ include/log-it/parts/Utils.hpp
@@ -2,7 +2,11 @@
 #ifndef _LOGIT_UITLS_HPP_INCLUDED
 #define _LOGIT_UITLS_HPP_INCLUDED
 /// \file Utils.hpp
-/// \brief
+/// \brief Aggregates various utility modules used throughout the project.
+///
+/// This header includes modules for formatting, handling variable values, argument parsing,
+/// encoding utilities, path utilities, and log record management. Including this header
+/// provides a single point of access to common utility functions and types.
 
 #include "Utils/format.hpp"
 #include "Utils/VariableValue.hpp"
