5f3a8bbf160a53af500a93a06aed654c5b9af797
Aster Seker <git.electroyar@gmail.com>
2025-09-12 09:50:39 +0300

    test: read log file via logger path

--- tests/file_logger_test.cpp
+++ tests/file_logger_test.cpp
@@ -1,6 +1,5 @@
 #define LOGIT_FILE_LOGGER_PATH "."
 #include <LogIt.hpp>
-#include <time_shield.hpp>
 #include <fstream>
 #include <string>
 
@@ -9,9 +8,8 @@ int main() {
     const std::string message = "test log message";
     LOGIT_INFO(message);
     LOGIT_WAIT();
-    const auto date_ts = time_shield::start_of_day(time_shield::ms_to_sec(time_shield::ts_ms()));
-    const std::string filename = time_shield::to_iso8601_date(date_ts) + ".log";
-    std::ifstream in(filename);
+    const std::string log_path = LOGIT_GET_LAST_FILE_PATH(0);
+    std::ifstream in(log_path);
     std::string line;
     std::getline(in, line);
     return line.find(message) != std::string::npos ? 0 : 1;
