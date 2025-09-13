e532381d794508f2ee16c902e9a1ae89c139eb9d
Aster Seker <git.electroyar@gmail.com>
2025-09-13 10:16:11 +0300

    refactor: update TaskExecutor.hpp

--- include/logit_cpp/logit/detail/TaskExecutor.hpp
+++ include/logit_cpp/logit/detail/TaskExecutor.hpp
@@ -13,7 +13,7 @@
 #include <iostream>
 #include <chrono>
 
-namespace logit::detail {
+namespace logit { namespace detail {
 
 #if defined(__EMSCRIPTEN__)
 
@@ -136,6 +136,6 @@ namespace logit::detail {
 
 #endif // defined(__EMSCRIPTEN__)
 
-} // namespace logit::detail
+}} // namespace logit::detail
 
 #endif // _LOGIT_DETAIL_TASK_EXECUTOR_HPP_INCLUDED
