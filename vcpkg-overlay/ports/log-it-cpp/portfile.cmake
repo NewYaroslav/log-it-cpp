02f64a1d6419e869af5a9f09dea4191769a23924
Aster Seker <git.electroyar@gmail.com>
2025-09-13 04:47:38 +0300

    Use pinned GitHub sources in vcpkg port

--- vcpkg-overlay/ports/log-it-cpp/portfile.cmake
+++ vcpkg-overlay/ports/log-it-cpp/portfile.cmake
@@ -1,14 +1,28 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO NewYaroslav/log-it-cpp
+    REF b1bceedddf34b417eb8b81de3508fe9c9704a46a
+    SHA512 f3781cbb4e9e190df38c3fe7fa80ba69bf6f9dbafb158e0426dd4604f2f1ba794450679005a38d0f9f1dad0696e2f22b8b086b2d7d08a0f99bb4fd3b0f7ed5d8
+    HEAD_REF main
+)
+
 vcpkg_cmake_configure(
-    SOURCE_PATH "${CURRENT_PORT_DIR}/../../.."
-    OPTIONS -DLOG_IT_CPP_BUILD_TESTS=OFF
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DLOG_IT_CPP_BUILD_TESTS=OFF
 )
 
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(
+    PACKAGE_NAME log-it-cpp
     CONFIG_PATH lib/cmake/log-it-cpp
 )
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
-vcpkg_install_copyright(FILE_LIST "${CURRENT_PORT_DIR}/../../../LICENSE")
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/lib"
+    "${CURRENT_PACKAGES_DIR}/debug/share/${PORT}"
+)
