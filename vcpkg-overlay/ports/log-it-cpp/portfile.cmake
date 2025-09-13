02ef4e9019da47be84ce3cedb50c0810792a8e24
Aster Seker <git.electroyar@gmail.com>
2025-09-13 06:47:14 +0300

    chore(vcpkg): pin log-it-cpp port to a5ef55f

--- vcpkg-overlay/ports/log-it-cpp/portfile.cmake
+++ vcpkg-overlay/ports/log-it-cpp/portfile.cmake
@@ -1,23 +1,19 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO NewYaroslav/log-it-cpp
-    REF 6ae5c3c0c84422695ade31c25191af812474ee93
-    SHA512 9045037107c54b3bf9a9f1c9e24968a827dbb77c0fd82c22aa5bfb5ca43ff3b533a51a5ca4f3a435db5df9ebfe58e00c2d5dd9f11aa3f61a0e3d0191ae0eb604
+    REF a5ef55f060f720063bbbb5c9dfdb57086d618e96
+    SHA512 1a5a418fcae653ab9731068a21c17a56577f98162d2c292418f60d2722ef3075c83f065c4b6115979dd1f38dd54d50f2ecb635ac0049cafd3cfc2a33aa708fff
     HEAD_REF main
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        -DLOG_IT_CPP_BUILD_TESTS=OFF
+    OPTIONS -DLOG_IT_CPP_BUILD_TESTS=OFF
 )
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(
-    PACKAGE_NAME log-it-cpp
-    CONFIG_PATH lib/cmake/log-it-cpp
-)
+vcpkg_cmake_config_fixup(PACKAGE_NAME log-it-cpp CONFIG_PATH lib/cmake/log-it-cpp)
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
