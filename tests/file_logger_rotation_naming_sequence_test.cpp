f0229eb42d592de214487a8ca3ac16a750fe0772
Aster Seker <git.electroyar@gmail.com>
2025-09-14 21:11:39 +0300

    test(file-logger): add rotation naming tests

---
+++ tests/file_logger_rotation_naming_sequence_test.cpp
@@ -0,0 +1,31 @@
+#include <LogIt.hpp>
+#include <regex>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+
+int main() {
+    std::system("rm -rf rotation_seq");
+    logit::FileLogger::Config cfg;
+    cfg.directory = "rotation_seq";
+    cfg.max_file_size_bytes = 20;
+    cfg.naming = logit::RotationNaming::Sequence;
+    logit::Logger::get_instance().add_logger(
+        std::unique_ptr<logit::FileLogger>(new logit::FileLogger(cfg)),
+        std::unique_ptr<logit::SimpleLogFormatter>(new logit::SimpleLogFormatter("%v")));
+    const std::string msg = "0123456789";
+    LOGIT_INFO(msg);
+    LOGIT_INFO(msg);
+    LOGIT_WAIT();
+    std::string current = LOGIT_GET_LAST_FILE_PATH(0);
+    LOGIT_SHUTDOWN();
+    size_t pos = current.rfind(".log");
+    if (pos == std::string::npos) return 1;
+    std::string rotated = current;
+    rotated.insert(pos, ".001");
+    std::string name = rotated.substr(rotated.find_last_of("/\\") + 1);
+    std::regex re("\\d{4}-\\d{2}-\\d{2}\\.001\\.log");
+    if (!std::regex_match(name, re)) return 1;
+    std::ifstream f(rotated.c_str());
+    return f.good() ? 0 : 1;
+}
