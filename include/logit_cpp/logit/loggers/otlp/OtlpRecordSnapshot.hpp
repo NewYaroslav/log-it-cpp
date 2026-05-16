85f0d5bbfd2cb1b4718feae3c556da5ed2ba1f01
Aster Seker <git.electroyar@gmail.com>
2026-05-16 10:33:22 +0300

    fix(otlp): make record snapshot header self-contained

--- include/logit_cpp/logit/loggers/otlp/OtlpRecordSnapshot.hpp
+++ include/logit_cpp/logit/loggers/otlp/OtlpRecordSnapshot.hpp
@@ -5,7 +5,7 @@
 /// \file OtlpRecordSnapshot.hpp
 /// \brief Defines a stable snapshot of LogRecord data for asynchronous OTLP export.
 
-#include "../../utils/LogRecord.hpp"
+#include "../../utils.hpp"
 #include <sstream>
 #include <string>
 #include <thread>
