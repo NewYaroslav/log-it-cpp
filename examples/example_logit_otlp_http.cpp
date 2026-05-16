b5ff63a1b37a26820c1fd357d38c48272287bbec
Aster Seker <git.electroyar@gmail.com>
2026-05-16 10:42:19 +0300

    docs(otlp): add OTLP HTTP logger example

---
+++ examples/example_logit_otlp_http.cpp
@@ -0,0 +1,32 @@
+#include <logit.hpp>
+
+int main() {
+#ifndef LOGIT_WITH_OTLP
+    LOGIT_ADD_CONSOLE_DEFAULT();
+    LOGIT_WARN("OTLP example requires LOGIT_WITH_OTLP=ON");
+    LOGIT_WAIT();
+    return 0;
+#else
+    logit::OtlpHttpLoggerConfig config;
+    config.host = "http://localhost:4318";
+    config.path = "/v1/logs";
+    config.service_name = "logit-otlp-example";
+    config.deployment_environment = "dev";
+    config.max_batch_size = 32;
+    config.export_interval_ms = 500;
+
+    LOGIT_ADD_LOGGER(
+        logit::OtlpHttpLogger,
+        (config),
+        logit::SimpleLogFormatter,
+        ("%v")
+    );
+
+    LOGIT_INFO("OTLP logger started");
+    LOGIT_WARN("Example warning message");
+    LOGIT_ERROR("Example error message");
+
+    LOGIT_WAIT();
+    return 0;
+#endif
+}
